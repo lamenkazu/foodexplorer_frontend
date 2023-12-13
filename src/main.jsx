dc952486014f7b7279a8a18acfd8e7b2c65a12c0
Erick Etiene <333.erick@gmail.com>
2023-12-13 07:56:24 -0300

    ✨ Routes Index Base on Authentication

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+
 import { GlobalStyles } from "./styles/global";
 import { ThemeProvider } from "styled-components";
 import { defaultTheme } from "./styles/theme";
+
+import { AuthProvider } from "./hooks/auth";
+
+import { Routes } from "./routes";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <ThemeProvider theme={defaultTheme}>
       <GlobalStyles />
-      <h1>Hello Food Explorer!</h1>
+      <AuthProvider>
+        <Routes />
+      </AuthProvider>
     </ThemeProvider>
   </React.StrictMode>
 );
---
+++ src/routes/index.jsx
@@ -0,0 +1,39 @@
+import { useEffect } from "react";
+import { useAuth } from "../hooks/auth";
+
+import { api } from "../services/api";
+import { USER_ROLE } from "./../utils/roles";
+
+import { BrowserRouter } from "react-router-dom";
+import { AuthRoutes } from "./auth.routes";
+import { AdminRoutes } from "./admin.routes";
+import { CustomerRoutes } from "./customer.routes";
+
+export const Routes = () => {
+  const { user, signOut } = useAuth();
+
+  useEffect(() => {
+    api.get("/Users/validate").catch((err) => {
+      if (err.response?.status === 401) signOut();
+    });
+  }, []);
+
+  return (
+    <BrowserRouter>
+      {user ? <AccessRoute user={user} /> : <AuthRoutes />}
+    </BrowserRouter>
+  );
+};
+
+const AccessRoute = ({ user }) => {
+  switch (user.role) {
+    case USER_ROLE.ADMIN:
+      return <AdminRoutes />;
+
+    case USER_ROLE.CUSTOMER:
+      return <CustomerRoutes />;
+
+    default:
+      return <CustomerRoutes />;
+  }
+};
