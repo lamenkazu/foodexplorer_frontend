e7cde38b4ff5fdb6f35f7f0e49067b3fb4043c76
Erick Etiene <333.erick@gmail.com>
2023-12-18 14:47:45 -0300

    ✏️ Footer Desktop

--- src/components/Footer/styles.js
+++ src/components/Footer/styles.js
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { DEVICE_BREAKPOINTS } from "../../styles/deviceBreakpoints";
 
 export const Container = styled.footer`
   display: flex;
@@ -16,5 +17,10 @@ export const Container = styled.footer`
     font-family: "DM Sans", sans-serif;
     font-size: 1.2rem;
     text-align: center;
+  }
+
+  @media (min-width: ${DEVICE_BREAKPOINTS.MD}) {
+    align-items: center;
+    padding-inline: 12.3rem;
   }
 `;
