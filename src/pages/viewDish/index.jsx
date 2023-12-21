40e23e4ff7f0f4691653653978f4c6bdf70a8801
Erick Etiene <333.erick@gmail.com>
2023-12-20 23:39:37 -0300

    ✏️ Loading State in Dish View && Empty Component

--- src/pages/viewDish/index.jsx
+++ src/pages/viewDish/index.jsx
@@ -1,6 +1,10 @@
 import { useState, useEffect } from "react";
 import { useNavigate, useParams } from "react-router-dom";
+
 import { useDishData } from "../../hooks/dishData";
+import { useAuth } from "../../hooks/auth";
+
+import { USER_ROLE } from "../../utils/roles";
 
 import {
   Container,
@@ -12,15 +16,20 @@ import {
   IngredientTags,
 } from "./styles";
 import { Stepper } from "./../../components/Stepper";
+import { Empty } from "../../components/Empty";
+
 import { PiCaretLeft, PiReceiptLight } from "react-icons/pi";
 import { IngredientTag } from "../../components/IngredientTag";
 
 export const ViewDish = () => {
   const { dish_id } = useParams();
   const navigate = useNavigate();
-  const { getDishById, getDishImage } = useDishData();
-  const [data, setData] = useState({});
 
+  const { user } = useAuth();
+  const { getDishById, getDishImage } = useDishData();
+
+  const [loading, setLoading] = useState(false);
+  const [data, setData] = useState({});
   const dishImg = getDishImage(data.image);
 
   const handleGoBack = () => {
@@ -31,6 +40,7 @@ export const ViewDish = () => {
     const fetchData = async () => {
       const response = await getDishById(dish_id);
       setData(response);
+      setLoading(true);
     };
 
     fetchData();
@@ -38,32 +48,45 @@ export const ViewDish = () => {
 
   return (
     <Container>
-      <GoBack onClick={handleGoBack}>
-        <PiCaretLeft size={32} />
-        <p>voltar</p>
-      </GoBack>
+      {loading ? (
+        <>
+          <GoBack onClick={handleGoBack}>
+            <PiCaretLeft size={32} />
+            <p>voltar</p>
+          </GoBack>
 
-      <Cover src={dishImg} alt="" />
+          <Cover src={dishImg} alt="" />
 
-      <ContentDetails>
-        <h1>{data.title}</h1>
+          <ContentDetails>
+            <h1>{data.title}</h1>
 
-        <p>{data.description}</p>
+            <p>{data.description}</p>
 
-        <IngredientTags>
-          {data?.ingredients?.map((ingredient) => (
-            <IngredientTag key={`${dish_id}${ingredient}`} title={ingredient} />
-          ))}
-        </IngredientTags>
-      </ContentDetails>
+            <IngredientTags>
+              {data?.ingredients?.map((ingredient) => (
+                <IngredientTag
+                  key={`${dish_id}${ingredient}`}
+                  title={ingredient}
+                />
+              ))}
+            </IngredientTags>
+          </ContentDetails>
 
-      <CustomerOrder>
-        <Stepper />
-        <StyledButton
-          icon={PiReceiptLight}
-          title={`pedir ∙ R$ ${data.price}`}
-        />
-      </CustomerOrder>
+          {![USER_ROLE.ADMIN].includes(user.role) ? (
+            <CustomerOrder>
+              <Stepper />
+              <StyledButton
+                icon={PiReceiptLight}
+                title={`pedir ∙ R$ ${data.price}`}
+              />
+            </CustomerOrder>
+          ) : (
+            <StyledButton title={`Editar Prato`} />
+          )}
+        </>
+      ) : (
+        <Empty />
+      )}
     </Container>
   );
 };
