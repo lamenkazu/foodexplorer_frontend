a41093ad919ed8a22582f63603394e2b7af6b01b
Erick Etiene <333.erick@gmail.com>
2024-01-05 11:16:27 -0300

    ✨ Using Deployed API

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,9 @@
 import axios from "axios";
 
+const devUrl = "http://localhost:3033";
+const productionUrl = "https://foodexplorer-backend-or5d.onrender.com";
+
 export const api = axios.create({
-  baseURL: "http://localhost:3033",
+  baseURL: productionUrl,
   withCredentials: true,
 });
